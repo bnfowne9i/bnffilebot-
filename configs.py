cce5f7512be0e4080cd9fcd257cd0bd11b858aaa
bnfowne9i <bnfowner@gmail.com>
2024-11-23 09:29:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,21 +1,21 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "24782172"))
+  API_HASH = os.environ.get("API_HASH", "ec1ef9cce1c4d852365e55e3139b72d8")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8095299226:AAGhMq1eSF6yDydQwfgAaCzeE53BHe7Gu44")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "@FileTo_LinkBot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002299322267"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "mdisk.pro")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "8df317bff1977ef01061b3c3b31fde785f4811e5")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6434831584"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://bharathkalladi38:4TDnaR1dZqAEshQq@cluster0.ps5ucul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002478941535")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002250961481"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1002389880858").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/FileTo_LinkBot)
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
@@ -32,11 +32,11 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/TK_Mob_TY)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/Tamil_Mob_TY)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
